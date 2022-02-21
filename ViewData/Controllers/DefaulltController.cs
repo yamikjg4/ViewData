3a17ef0afe12e6ff3a9980ae21cbb7bd765989d9
yamikjg4 <yamikgandhi@outlook.com>
2022-02-21 14:06:15 +0530

    new

---
+++ ViewData/Controllers/DefaulltController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewData.Controllers
+{
+    public class DefaulltController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Yamik Gandhi";
+            TempData["fname"] = "Mahesh";
+            return View();
+        }
+        public IActionResult viewuser()
+        {
+            return View();
+        }
+    }
+}
